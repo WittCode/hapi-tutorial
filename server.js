0d5eab4d48ac73f8073250bab75fc5e88530a367
Tom Wittmann <WittCode@gmail.com>
2021-02-05 17:12:08 -0500

    Added inert plugin and welcome.html.

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const path = require('path');
 
 const init = async () => {
 
     const server = Hapi.Server({
         host: 'localhost',
-        port: 1234
+        port: 1234,
+        routes: {
+            files: {
+                relativeTo: path.join(__dirname, 'static')
+            }
+        }
     });
 
-    await server.register({
+    await server.register([{
         plugin: require('hapi-geo-locate'),
         options: {
             enabledByDefault: false
         }
-    });
+    },
+    {
+        plugin: Inert
+    }]);
 
     server.route([{
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-            return "<h1>Hello World!</h1>"
+            return h.file('welcome.html');
+        }
+    },
+    {
+        method: 'GET',
+        path: '/download',
+        handler: (request, h) => {
+            return h.file('welcome.html', {
+                mode: 'inline',
+                filename: 'welcome-download.html'
+            });
         }
     },
     {
@@ -38,9 +58,7 @@ const init = async () => {
         method: 'GET',
         path: '/users/{soccer?}',
         handler: (request, h) => {
-
-            return h.redirect('/');
-
+            return "<h1>USERS PAGE</h1>";
         }
     },
     {
